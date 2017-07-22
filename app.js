97add01fe6efc52bf4cfb79e2c18f2d6e1f20ca4
goroh83 <goroh83@yahoo.com>
2017-07-22 15:27:17 +0000

    add basics(form, routes, pics array

---
+++ app.js
@@ -0,0 +1,44 @@
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser');
+    
+app.use(bodyParser.urlencoded({extended: true}));    
+app.set('view engine', 'ejs');
+
+//LANDING page
+app.get('/', function(req, res){
+    res.render('landing');
+});
+
+var photos = [
+    {name: "Hangzhou", image: "http://photo.goroh.co/wp-content/uploads/2017/02/DSC_2751.jpg"},
+    {name: "Shanghai", image: "http://photo.goroh.co/wp-content/uploads/2017/02/DSC_3899.jpg"},
+    {name: "Guangzhou", image: "http://photo.goroh.co/wp-content/uploads/2017/02/P6303523.jpg"}
+];
+
+
+// SHOW all photos
+app.get('/photos', function(req, res){
+    res.render('photos', {photos: photos});
+});
+
+// CREATE  new photo
+app.post('/photos', function(req, res){
+    // get data from form
+    var name = req.body.name;
+    var image = req.body.image;
+    var newPhoto = { name: name, image: image }
+    // add to photos array
+    photos.push(newPhoto);
+    // redirect back to photos page
+    res.redirect('/photos');
+});
+
+// SUBMIT new photo
+app.get('/photos/new', function(req, res){
+    res.render('new');
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('server started');
+})
